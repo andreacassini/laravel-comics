303e381981bde2453114140de2fa7b8e1e1e7f26
andreacassini <andrea1316@hotmail.it>
2023-07-22 17:10:35 +0200

    macrostructure-header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,17 @@
 <header>
-    
+   <div class="container">
+    <div class="row py-3">
+        <nav class="col-12 d-flex align-items-center justify-content-between">
+            <img src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
+            <ul class="text-uppercase list-unstyled">
+                <li>
+                    <a href="">characters</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              </form>
+        </nav>
+    </div>
+   </div>
 </header>
