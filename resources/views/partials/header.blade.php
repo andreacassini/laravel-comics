35b810aa5988d617a3f885a46dffa486fa55dc59
andreacassini <andrea1316@hotmail.it>
2023-07-23 17:21:13 +0200

    style header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,38 +1,48 @@
 <header>
     <div class="container">
-        <div class="row py-3">
+        <div class="row">
             <nav class="col-12 d-flex align-items-center justify-content-between">
                 <img src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
-                <ul class="text-uppercase list-unstyled d-flex my-2 aling-items-center">
+                <ul class="text-uppercase list-unstyled d-flex aling-items-center" id="header-ul">
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'characters' ? 'color-primary' : ''}} mx-2" href="">characters</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'characters' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">characters</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'comics' ? 'color-primary' : ''}} mx-2" href="">comics</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'comics' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">comics</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'moveis' ? 'color-primary' : ''}} mx-2" href="">moveis</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'moveis' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">moveis</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'tv' ? 'color-primary' : ''}} mx-2" href="">tv</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'tv' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">tv</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'games' ? 'color-primary' : ''}} mx-2" href="">games</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'games' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">games</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'collectibles' ? 'color-primary' : ''}} mx-2" href="">collectibles</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'collectibles' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">collectibles</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'videos' ? 'color-primary' : ''}} mx-2" href="">videos</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'videos' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">videos</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'fans' ? 'color-primary' : ''}} mx-2" href="">fans</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'fans' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">fans</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'news' ? 'color-primary' : ''}} mx-2" href="">news</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'news' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">news</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ Route::currentRouteName() === 'shop' ? 'color-primary' : ''}} mx-2" href="">shop</a>
+                        <a class="nav-link {{ Route::currentRouteName() === 'shop' ? 'color: #0282F9;
+                        border-bottom: 4px solid #0282F9' : ''}} mx-2" href="">shop</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
