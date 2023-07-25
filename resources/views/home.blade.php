54bdd4bda2a848667a04649bc3121e9aeac92e55
andreacassini <andrea1316@hotmail.it>
2023-07-25 20:27:01 +0200

    done yield EXTENDS SECTON HOME content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Laravel Comics</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include ('partials.header')
-    <main>
-        <div id="jumbotron">
-        </div>
-    </main>
-    @include ('partials.footer')
-</body>
-
-</html>
+@extends('layouts.app')  {{-- HOME-CONTENT --}}
+@section('name') 
+    <div id="jumbotron">
+    </div>
+@endsection
