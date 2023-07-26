1ab46409ba6cb776d283477b55a16918e689867f
andreacassini <andrea1316@hotmail.it>
2023-07-26 20:28:33 +0200

    linked all from comics es base done, need style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,18 +1,93 @@
 @extends('layouts.app')
 
 @section('home-content')
+    <div id="jumbotron"></div>
     <div class="container">
-        <div class="row mb-5">
+        <div class="row my-5">
             <div class="col-12">
-                <div class="row">
-                    <div class="col-6">
-                        <img src="{{$comic['thumb']}}" alt="">
-                    </div>
-                    <div class="col-6">
-                        <h3><strong>{{$comic['title']}}</strong></h3>
-                    </div>
+                <div>
+                    <img src="{{$comic['thumb']}}" alt="">
                 </div>
-                
+                <div>
+                    <h3><strong>{{$comic['title']}}</strong></h3>
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    <strong>{{$comic['price']}}</strong>
+                </div>
+                <div class="row justify-content-between">
+                    {{-- PRIMA COLONNA --}}
+                    <div class="col-6">
+                        <h3>Talent</h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-3">
+                                <p><strong>
+                                    Art by: 
+                                </strong>
+                                </p>
+                            </div>
+                            <div class="col-9">
+                                @foreach ($comic['artists'] as $index => $artist)
+                                {{$artist}},
+                                @endforeach
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-3">
+                                <p><strong>
+                                    Written by: 
+                                </strong>
+                                </p>
+                            </div>
+                            <div class="col-9">
+                                @foreach ($comic['writers'] as $index => $writer)
+                                {{$writer}},
+                                @endforeach
+                            </div>
+                        </div>
+                        <hr>
+                        {{-- SECONDA COLONNA --}}
+                        <div class="col-6">
+                            <h3>Specs</h3>
+                            <hr>
+                            <div class="row">
+                                <div class="col-4">
+                                    <p><strong>
+                                        Series: 
+                                    </strong>
+                                    </p>
+                                </div>
+                                <div class="col-8 text-uppercase">
+                                    {{$comic['series']}}
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <p><strong>
+                                        U.S. Price: 
+                                    </strong>
+                                    </p>
+                                </div>
+                                <div class="col-8">
+                                    {{$comic['price']}}
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <p><strong>
+                                        Series: 
+                                    </strong>
+                                    </p>
+                                </div>
+                                <div class="col-8">
+                                    {{$comic['sale_date']}}
+                                </div>
+                            </div>
+                </div>
             </div>
         </div>
     </div>
